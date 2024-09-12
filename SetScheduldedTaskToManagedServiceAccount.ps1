b3aa5226d3689c1b7d9463db8f2193b87334c707
Ben <benjamin+github@b45h.de>
2024-09-12 16:51:12 +0200

    Update SetScheduldedTaskToManagedServiceAccount.ps1

--- SetScheduldedTaskToManagedServiceAccount.ps1
+++ SetScheduldedTaskToManagedServiceAccount.ps1
@@ -2,5 +2,6 @@
 #For the login of the task use a normal user
 #Now run the following command to set you managed service account user for the planned task
 #After a refresh the managedserviceaccount should be listet as the user used to run the task
+(It might be neccassary to open cmd as an administrator)
 
 schtasks /Change /TN ScheduledTaskName /RU "ManagedServiceAccount$" /RP ""
